0185c8b400993298893395b9966796c7749c72ce
GEND1T <akunumum1606@gmail.com>
2025-12-14 23:00:09 +0700

    Add service worker install event listener

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
-self.addEventListener('fetch', function(event) {
-    // Kosong: Biarkan semua request lewat (network only)
+// File: sw.js di GitHub
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Biarkan request lewat (Network Only) agar aplikasi selalu online
 });
